d415ef0c99b6be0a733a948874333a6babefe964
Vinicius Barbosa <viniciusmbarbosa@hotmail.com>
2022-09-27 21:57:01 -0300

    feat: changing state

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -5,11 +5,20 @@ const INITIAL_STATE: ICartState = {
   items: [],
 }
 const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
-  console.log(state, action)
   switch (action.type) {
     case 'ADD_PRODUCT_TO_CART': {
-      console.log(action.payload)
-      return state
+      const { product } = action.payload
+
+      return {
+        ...state,
+        items: [
+          ...state.items,
+          {
+            product,
+            quantity: 1,
+          },
+        ],
+      }
     }
     default: {
       return state
