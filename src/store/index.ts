ea1438bfb1056201a460554d1ad1b69919d023a6
Vinicius Barbosa <viniciusmbarbosa@hotmail.com>
2022-09-27 21:50:18 -0300

    fix: fixing createStore

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,6 @@
 import { createStore } from 'redux'
 import rootReducer from './modules/rootReducer'
 
-const store = createStore(() => rootReducer)
+const store = createStore(rootReducer)
 
 export default store
