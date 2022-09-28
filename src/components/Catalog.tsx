bdcc70663ee8c72e3f10b91b459b9e85df74047d
Vinicius Barbosa <viniciusmbarbosa@hotmail.com>
2022-09-27 22:46:48 -0300

    feat: abstracting item from catalog

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,11 +1,9 @@
 import React, { useEffect, useState } from 'react'
-import { useDispatch } from 'react-redux'
 import { api } from '../services/api'
-import { addProductToCart } from '../store/modules/cart/actions'
 import { IProduct } from '../store/modules/cart/types'
+import { CatalogItem } from './CatalogItem'
 
 export const Catalog: React.FC = () => {
-  const dispatch = useDispatch()
   const [catalog, setCatalog] = useState<IProduct[]>([])
 
   useEffect(() => {
@@ -14,24 +12,12 @@ export const Catalog: React.FC = () => {
     })
   }, [])
 
-  const handleAddProductToCart = (product: IProduct) => {
-    dispatch(addProductToCart(product))
-  }
-
   return (
     <main>
       <h1>Catalog</h1>
 
       {catalog.map((product) => (
-        <article key={product.id}>
-          <strong>{product.title}</strong>
-          {' - '}
-          <span>{product.price}</span>
-          {'  '}
-          <button type="button" onClick={() => handleAddProductToCart(product)}>
-            Comprar
-          </button>
-        </article>
+        <CatalogItem key={product.id} product={product} />
       ))}
     </main>
   )
---
+++ src/components/CatalogItem.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { addProductToCart } from '../store/modules/cart/actions'
+import { IProduct } from '../store/modules/cart/types'
+
+interface ICatalogItemProps {
+  product: IProduct
+}
+
+export const CatalogItem: React.FC<ICatalogItemProps> = ({ product }) => {
+  const dispatch = useDispatch()
+
+  const handleAddProductToCart = () => {
+    dispatch(addProductToCart(product))
+  }
+
+  return (
+    <article key={product.id}>
+      <strong>{product.title}</strong>
+      {' - '}
+      <span>{product.price}</span>
+      {'  '}
+      <button type="button" onClick={handleAddProductToCart}>
+        Comprar
+      </button>
+    </article>
+  )
+}
